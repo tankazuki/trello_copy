0cfa8851d93c53075afc87de269a27c11c36cab2
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-04 10:17:27 +0900

    settingの設定を修正

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'trello_copy.apps.KanbanConfig',
+    'kanban.apps.KanbanConfig',
 ]
 
 MIDDLEWARE = [
