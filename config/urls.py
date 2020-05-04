1180e7bcaf484ea7a748771c54bc11bddb38c61e
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-04 11:00:50 +0900

    ログイン機能の実装

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('kanban/', include('kanban.urls')),
+    path('kanban/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
