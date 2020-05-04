a00696fb72c93d4dc708e77a14f44b724de57102
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-04 10:24:19 +0900

    認証完了ページとルーティングを追加

--- kanban/urls.py
+++ kanban/urls.py
@@ -6,4 +6,5 @@ app_name = "kanban"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
 ]
--- kanban/views.py
+++ kanban/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "kanban/index.html")
+
+
+def home(request):
+    return render(request, "kanban/home.html")
