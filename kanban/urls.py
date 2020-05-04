45c050704d7aef075c72863f7424f341c73dd19d
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-04 10:39:58 +0900

    signupページの実装

--- kanban/urls.py
+++ kanban/urls.py
@@ -7,4 +7,5 @@ app_name = "kanban"
 urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
 ]
--- kanban/views.py
+++ kanban/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -7,3 +9,16 @@ def index(request):
 
 def home(request):
     return render(request, "kanban/home.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("kanban:home")
+    else:
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "kanban/signup.html", context)
