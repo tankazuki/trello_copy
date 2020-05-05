b8ce5eaef5faf776e837ded9fbf6ea1b0f803884
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-05 12:34:16 +0900

    cssを追加してUIの改善

--- kanban/forms.py
+++ kanban/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+
 from .models import List, Card
 
 
 class UserForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
@@ -12,6 +18,11 @@ class UserForm(forms.ModelForm):
 
 class ListForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(ListForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = List
         fields = ("title",)
@@ -19,13 +30,35 @@ class ListForm(forms.ModelForm):
 
 class CardForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(CardForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Card
-        fields = ("title", "description", "list",)
+        fields = ("title", "description", "list")
 
 
 class CardCreateFromHomeForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Card
         fields = ("title", "description",)
+
+
+class CardUpdateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardUpdateForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
